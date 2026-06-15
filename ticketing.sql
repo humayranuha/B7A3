81a86b3c86ebc553630a8e8575a356c91fe33f60
humayranuha <nuhahumayra24@gmail.com>
2026-06-15 21:30:43 +0600

    feat: users table added

--- ticketing.sql
+++ ticketing.sql
@@ -2,3 +2,21 @@
 DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
+
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
+CREATE TABLE Users (
+    user_id INT,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    role VARCHAR(50),
+    phone_number VARCHAR(20),
+    
+    -- Primary Key constraint
+    CONSTRAINT pk_users PRIMARY KEY (user_id),
+    -- Unique email constraint
+    CONSTRAINT uk_users_email UNIQUE (email),
+    -- Check constraint for role
+    CONSTRAINT chk_users_role CHECK (role IN ('Football Fan', 'Ticket Manager'))
+);
