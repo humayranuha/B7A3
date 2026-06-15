07b6a165b7ac8b27c14d82daedabfa1e50b38a5e
humayranuha <nuhahumayra24@gmail.com>
2026-06-15 21:43:00 +0600

    feat: query 6 added

--- ticketing.sql
+++ ticketing.sql
@@ -213,3 +213,9 @@ SELECT
 FROM Users u
 LEFT JOIN Bookings b ON u.user_id = b.user_id
 ORDER BY u.user_id, b.booking_id;
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher 
+-- than the average cost of all ticket bookings.
+SELECT booking_id, match_id, total_cost
+FROM Bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings);
